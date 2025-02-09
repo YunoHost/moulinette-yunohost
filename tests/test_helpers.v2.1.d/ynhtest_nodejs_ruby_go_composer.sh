62ca7209fd659930b1e12a1830862543767f93b0
Alexandre Aubin <alex.aubin@mailoo.org>
2025-02-10 00:45:14 +0100

    friskies

--- tests/test_helpers.v2.1.d/ynhtest_nodejs_ruby_go_composer.sh
+++ tests/test_helpers.v2.1.d/ynhtest_nodejs_ruby_go_composer.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 ynhtest_nodejs_install() {
+    local install_dir="$(mktemp -d -p "$VAR_WWW")"
+
     nodejs_version=20
     ynh_nodejs_install
 
     node --version | grep -q '^v20\.'
 
-    install_dir="$(mktemp -d -p "$VAR_WWW")"
     pushd $install_dir
         # Install a random simple package to validate npm is in the path and working
         npm install ansi-styles
@@ -16,13 +17,14 @@ ynhtest_nodejs_install() {
 }
 
 ynhtest_ruby_install() {
+    local install_dir="$(mktemp -d -p "$VAR_WWW")"
+
     ruby_version=3.3.5
     ynh_ruby_install
 
     ruby --version
     ruby --version | grep '^3\.3\.5'
 
-    install_dir="$(mktemp -d -p "$VAR_WWW")"
     pushd $install_dir
         # FIXME: should test installing as non-root with ynh_exec_as_app to validate PATH propagation ?
         gem install bundler passenger --no-document
@@ -31,13 +33,14 @@ ynhtest_ruby_install() {
 }
 
 ynhtest_go_install() {
+    local install_dir="$(mktemp -d -p "$VAR_WWW")"
+
     go_version=1.22
     ynh_go_install
 
     go version
     go version | grep 'go1.22 linux'
 
-    install_dir="$(mktemp -d -p "$VAR_WWW")"
     pushd $install_dir
         # FIXME: should test building as non-root with ynh_exec_as_app to validate PATH propagation ?
         cat << EOF > helloworld.go
@@ -52,7 +55,10 @@ EOF
 }
 
 ynhtest_composer_install() {
+    local install_dir="$(mktemp -d -p "$VAR_WWW")"
+
     composer_version="2.8.3"
+    php_version=8.2
 
     install_dir="$(mktemp -d -p "$VAR_WWW")"
     pushd $install_dir
